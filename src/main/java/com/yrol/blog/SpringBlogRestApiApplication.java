0a475d06583e310bc31eac92f47f45ffb130c282
Yrol <yrollgayanth@yahoo.com>
2022-10-23 21:47:18 +1100

    Renaming the main package and change spring version to 2.7.4

--- src/main/java/com/yrol/blog/SpringBlogRestApiApplication.java
+++ src/main/java/com/yrol/blog/SpringBlogRestApiApplication.java
@@ -1,4 +1,4 @@
-package com.yrol.blog.springblogrestapi;
+package com.yrol.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
