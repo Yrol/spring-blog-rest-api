31313b46a134127d573384094ec1ec74514ec2a2
Yrol <yrollgayanth@yahoo.com>
2022-10-23 20:38:19 +1100

    initialize project

---
+++ src/main/java/com/yrol/blog/springblogrestapi/SpringBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.yrol.blog.springblogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yrol/blog/springblogrestapi/SpringBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrol.blog.springblogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
