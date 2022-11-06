08814ffd945673a26703ced5957394f157b87b01
Yrol <yrollgayanth@yahoo.com>
2022-11-06 18:27:36 +1100

    Adding JWT dependancy to the app

---
+++ src/main/java/com/yrol/blog/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.yrol.blog.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * This class will handle unauthorized access security exception and will add as component to the spring context.
+ * Implements teh basic class AuthenticationEntryPoint
+ * The "commence" method will throw an exception if unauthorized access is detected
+ * */
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
